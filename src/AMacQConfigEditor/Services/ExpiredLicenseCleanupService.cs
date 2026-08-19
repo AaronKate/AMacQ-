41da6a2f3838e0057d1c6409525170cb3db0be99
AllenWade <792945414@qq.com>
2026-08-19 09:04:50 +0800

    修改授权过期后删除文件的逻辑

--- src/AMacQConfigEditor/Services/ExpiredLicenseCleanupService.cs
+++ src/AMacQConfigEditor/Services/ExpiredLicenseCleanupService.cs
@@ -1,18 +1,9 @@
-using System.IO;
-
 namespace AMacQConfigEditor.Services;
 
 internal static class ExpiredLicenseCleanupService
 {
-    public static void RemoveRuntimeConfigurationFiles()
+    public static void DisableRuntimeConfigurationFiles()
     {
-        var directory = ObscuredPackageDeploymentService.GetInstallDirectory();
-        if (directory is null) return;
-
-        foreach (var fileName in new[] { "sorinkg.lua", "sorinxs.lua" })
-        {
-            var path = Path.Combine(directory, fileName);
-            if (File.Exists(path)) File.Delete(path);
-        }
+        ObscuredPackageDeploymentService.DisableRuntimeConfigurationFiles();
     }
 }
